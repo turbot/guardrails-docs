e94eb4da3bbd1d31a3d8ca8afc82984bb4cec011
Victor Hadianto <victor@turbot.com>
2026-02-26 14:27:40 +1100

    wip

--- scripts/build-docs-export.js
+++ scripts/build-docs-export.js
@@ -62,7 +62,7 @@ function parseArgs() {
 
   if (!opts.version) {
     const now = new Date();
-    opts.version = `${now.getFullYear()}.${String(now.getMonth() + 1).padStart(2, "0")}.${String(now.getDate()).padStart(2, "0")}`;
+    opts.version = `${now.getFullYear()}.${String(now.getMonth() + 1).padStart(2, "0")}.${String(now.getDate()).padStart(2, "0")}.${String(now.getHours()).padStart(2, "0")}${String(now.getMinutes()).padStart(2, "0")}${String(now.getSeconds()).padStart(2, "0")}`;
   }
 
   return opts;
